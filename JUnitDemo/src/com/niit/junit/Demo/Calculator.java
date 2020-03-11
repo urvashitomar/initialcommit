ba9fc4eb3a25d41fb96e544663e398c5aa7ed22a
Urvashi <35293878+urvashitomar@users.noreply.github.com>
2020-03-11 15:57:16 +0530

    Update Calculator.java

--- JUnitDemo/src/com/niit/junit/Demo/Calculator.java
+++ JUnitDemo/src/com/niit/junit/Demo/Calculator.java
@@ -1,19 +1,19 @@
 package com.niit.junit.Demo;
 
 public class Calculator {
-	public double add(double a,double b)
+	public static double add(double a,double b)
 	{
 		return a+b;
 	}
-	public double subtract(double a,double b)
+	public static double subtract(double a,double b)
 	{
 		return a-b;
 	}
-	public double multiply(double a,double b)
+	public static double multiply(double a,double b)
 	{
 		return a*b;
 	}
-	public double divide(double a,double b)
+	public static double divide(double a,double b)
 	{	if(b==0)
 			throw new ArithmeticException();
 		return a/b;
